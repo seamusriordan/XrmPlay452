813742cf2026bb786e96521e0321208071868cdf
Seamus Riordan <seamus@riordan.io>
2021-01-25 05:33:20 -0600

    Add in tests with 365 dependency

---
+++ XrmPlay452.Tests/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace XrmPlay452.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
