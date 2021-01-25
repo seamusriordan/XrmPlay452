01b92688a9d95ead85bf95132aa0a59098a3d799
Seamus Riordan <seamus@riordan.io>
2021-01-25 05:36:57 -0600

    Add in tests from other project

---
+++ XrmPlay452.Tests/DoubleUpdaterTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Linq;
+using FakeItEasy;
+using FakeXrmEasy;
+using Microsoft.Xrm.Sdk;
+using Xunit;
+
+namespace XrmPlay452.Tests
+{
+    public class DoubleUpdaterTests
+    {
+        private readonly XrmFakedContext _fakeContext;
+        private readonly DoubleUpdater _doubleUpdater;
+        private const int InitialNumber = 7;
+        private const int FirstUpdatedNumber = 4;
+        private const int SecondUpdatedNumber = 3;
+
+        public DoubleUpdaterTests()
+        {
+            _fakeContext = new XrmFakedContext();
+
+            var fakeService = _fakeContext.GetOrganizationService();
+
+            var entity = new Entity(DoubleUpdater.EntityLogicalName)
+            {
+                [DoubleUpdater.AttributeName] = InitialNumber
+            };
+            fakeService.Create(entity);
+
+            _doubleUpdater = new DoubleUpdater(fakeService);
+        }
+
+        [Fact]
+        public void DoesNotHaveOldEntity()
+        {
+            _doubleUpdater.UpdateWithTwoNumbers(FirstUpdatedNumber, SecondUpdatedNumber);
+
+            var returnedInitialEntitiesAfterUpdate =
+                from thing in _fakeContext.CreateQuery(DoubleUpdater.EntityLogicalName)
+                where thing[DoubleUpdater.AttributeName] as int? == InitialNumber
+                select thing;
+            Assert.Equal(0, returnedInitialEntitiesAfterUpdate.Count());
+        }
+
+        [Fact]
+        public void HasFinalNewEntity()
+        {
+            _doubleUpdater.UpdateWithTwoNumbers(FirstUpdatedNumber, SecondUpdatedNumber);
+
+            var returnedUpdatedEntities = from thing in _fakeContext.CreateQuery(DoubleUpdater.EntityLogicalName)
+                where thing[DoubleUpdater.AttributeName] as int? == SecondUpdatedNumber
+                select thing;
+            Assert.Equal(1, returnedUpdatedEntities.Count());
+        }
+
+        [Fact]
+        public void CalledTwoUpdates()
+        {
+            _doubleUpdater.UpdateWithTwoNumbers(FirstUpdatedNumber, SecondUpdatedNumber);
+
+            var fakeService = _fakeContext.GetOrganizationService();
+            var calls = Fake.GetCalls(fakeService).ToList();
+            Assert.Equal(2, calls.Count(call =>
+                    call.Method.Name == "Update"
+                )
+            );
+        }
+
+        [Fact]
+        public void CalledUpdateUsingFirstNumber()
+        {
+            var fakeService = _fakeContext.GetOrganizationService();
+            Fake.ClearRecordedCalls(fakeService);
+
+            _doubleUpdater.UpdateWithTwoNumbers(FirstUpdatedNumber, SecondUpdatedNumber);
+
+            var calls = Fake.GetCalls(fakeService).ToList();
+            Assert.Contains(calls, call =>
+                call.Method.Name == "Update" &&
+                call.GetArgument<Entity>(0)?[DoubleUpdater.AttributeName] as int? == FirstUpdatedNumber
+            );
+        }
+    }
+}
---
+++ XrmPlay452/DoubleUpdater.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+
+namespace XrmPlay452
+{
+    public class DoubleUpdater
+    {
+        private readonly IOrganizationService _service;
+        public const string EntityLogicalName = "things";
+        public const string AttributeName = "beepboop";
+
+        public DoubleUpdater(IOrganizationService service)
+        {
+            _service = service;
+        }
+
+        public void UpdateWithTwoNumbers(int firstNumber, int second)
+        {
+            UpdateEntityContainingNumberToNumber(7, firstNumber);
+            UpdateEntityContainingNumberToNumber(firstNumber, second);
+        }
+
+        private void UpdateEntityContainingNumberToNumber(int initialNumber, int updatedNumber)
+        {
+            using (var context = new OrganizationServiceContext(_service))
+            {
+                var returnedInitialEntities = from thing in context.CreateQuery(EntityLogicalName)
+                    where (int) thing[AttributeName] == initialNumber
+                    select thing;
+
+                foreach (var entityToUpdate in returnedInitialEntities)
+                {
+                    UpdateEntityWithNumber(entityToUpdate, updatedNumber);
+                }
+            }
+        }
+
+        private void UpdateEntityWithNumber(Entity entity, int number)
+        {
+            entity[AttributeName] = number;
+            _service.Update(entity);
+        }
+    }
+}
